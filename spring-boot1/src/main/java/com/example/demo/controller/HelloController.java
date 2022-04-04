9eb1edb07f1fb8a73a0ec5cd199c80bf9a05677f
masatonara83 <masato.nara@rakus-partners.co.jp>
2022-04-04 15:57:30 +0900

    firstCommit

---
+++ spring-boot1/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+	@GetMapping("/")
+	public String greeting() {
+		return "form";
+	}
+	
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(required = false) String message, Model model) {
+		model.addAttribute("sample", message);
+		return "hello";
+		
+	}
+	
+	@PostMapping("/confirm")
+	public String confirm(@RequestParam String message, Model model) {
+		model.addAttribute("message", message);
+		return "confirm";
+	}
+}
