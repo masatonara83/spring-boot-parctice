e946aa3222e24a0fa0b5312cceb0600fc5d8b25c
masatonara83 <masato.nara@rakus-partners.co.jp>
2022-04-05 16:38:58 +0900

    aopによるログ出力を学習

---
+++ spring-ioc-aop/src/main/java/com/example/demo/SpringIocAopApplication.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@SpringBootApplication
+public class SpringIocAopApplication implements ApplicationRunner {
+	
+	private final ApplicationContext appContext;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIocAopApplication.class, args);
+	}
+
+	//IoCコンテナに登録されているオブジェクトを確認している
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		var allBeanNames = appContext.getBeanDefinitionNames();
+		for(var beanName : allBeanNames) {
+			log.info("Bean名: {}", beanName);
+		}
+		
+	}
+
+}
